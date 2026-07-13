aabcb6d6a89180c00622b546a38ac90302bf62ce
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-07-13 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/ControllerPlugin/SettingsListFactory.php
+++ src/Service/ControllerPlugin/SettingsListFactory.php
@@ -3,7 +3,7 @@
 namespace ApiInfo\Service\ControllerPlugin;
 
 use ApiInfo\Mvc\Controller\Plugin\SettingsList;
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class SettingsListFactory implements FactoryInterface
