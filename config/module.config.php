b298d0eb1f427205c0bf026e04285c7f1ab0c541
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-07-13 00:00:00 +0000

    Updated languages.

--- config/module.config.php
+++ config/module.config.php
@@ -47,4 +47,14 @@ return [
             ],
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => \Laminas\I18n\Translator\Loader\Gettext::class,
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
 ];
